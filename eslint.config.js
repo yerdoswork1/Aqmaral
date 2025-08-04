abb1eca24baf18dabd96aaa1c8db80b2745d3005
Aleksandr Krivoshein <megalistnn@gmail.com>
2025-08-05 03:52:01 +0400

    core: add prettier and husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { globalIgnores } from 'eslint/config';
 
-export default tseslint.config([
+export default [
   globalIgnores(['dist']),
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
+
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
+      parser: tseslint.parser,
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      globals: globals.browser
     },
-  },
-])
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      import: importPlugin,
+      prettier: prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+      'import/order': [
+        'warn',
+        {
+          groups: [['builtin', 'external'], 'internal', ['sibling', 'parent'], 'index'],
+          alphabetize: { order: 'asc', caseInsensitive: true }
+        }
+      ],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+    }
+  }
+];
