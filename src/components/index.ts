70b1ffe39503fa793af8a0c6ba17a27d9d673fe0
Aleksandr Krivoshein <77492375+Wood85@users.noreply.github.com>
2025-08-07 02:27:13 +0400

    feat: add info cards (#8)
    * docs: fill in README.md file
    
    * feat: add info cards

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './Spinner';
 export * from './Footer';
 export * from './Header';
 export * from './Banner';
+export * from './InfoCard';
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,25 @@
-// import { useTranslation } from 'react-i18next';
-
-import { Banner } from '@components/index';
+import { Banner, InfoCard } from '@components/index';
+import { useTranslation } from 'react-i18next';
 
 import styles from './Home.module.scss';
 
 export const Home = () => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
+  const INFO = [
+    { value: '13+', text: t('home.infoCards.experience') },
+    { value: '3', text: t('home.infoCards.countries') },
+    { value: '100+', text: t('home.infoCards.employees') },
+    { value: '200+', text: t('home.infoCards.projects') }
+  ];
   return (
     <main className={styles.container}>
       <section className={styles.banner}>
         <Banner />
+      </section>
+      <section className={styles.infoCards}>
+        {INFO.map((card, index) => (
+          <InfoCard key={index} value={card.value} text={card.text} />
+        ))}
       </section>
     </main>
   );
