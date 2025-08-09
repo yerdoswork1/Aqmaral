9d66432edae5f47362691dc49a7f4bedfb3687c2
Aleksandr Krivoshein <megalistnn@gmail.com>
2025-08-09 18:24:19 +0400

    feat: add not-found page

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,31 +1,37 @@
 import { Spinner } from '@components/Spinner';
 import { RouteNames } from '@config/RoutNames';
 import { AppLayout } from '@layouts/index';
-import { HomePage, ContactsPage, ProductPage, InfoPage } from '@pages/index';
+import { HomePage, ContactsPage, ProductPage, InfoPage, NotFoundPage } from '@pages/index';
 import { Suspense } from 'react';
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router';
 import { Navigate } from 'react-router-dom';
+
+const SUPPORTED_LANGS = ['ru', 'en', 'kk'];
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route
         index
-        element={<Navigate to={`/${localStorage.getItem('i18nextLng') || 'ru'}`} replace />}
+        element={<Navigate to={`/${localStorage.getItem('i18nextLng') || 'en'}`} replace />}
       />
-      <Route
-        path="/:lang"
-        element={
-          <Suspense fallback={<Spinner />}>
-            <AppLayout />
-          </Suspense>
-        }
-      >
-        <Route index path={RouteNames.MAIN} element={<HomePage />} />
-        <Route path={RouteNames.CONTACTS} element={<ContactsPage />} />
-        <Route path={RouteNames.PRODUCT} element={<ProductPage />} />
-        <Route path={RouteNames.INFO} element={<InfoPage />} />
-      </Route>
+      {SUPPORTED_LANGS.map((lang) => (
+        <Route
+          key={lang}
+          path={`/${lang}`}
+          element={
+            <Suspense fallback={<Spinner />}>
+              <AppLayout />
+            </Suspense>
+          }
+        >
+          <Route index path={RouteNames.MAIN} element={<HomePage />} />
+          <Route path={RouteNames.CONTACTS} element={<ContactsPage />} />
+          <Route path={RouteNames.PRODUCT} element={<ProductPage />} />
+          <Route path={RouteNames.INFO} element={<InfoPage />} />
+        </Route>
+      ))}
+      <Route path="*" element={<NotFoundPage />} />
     </>
   )
 );
