09ebb74bc1692ce98eca8c63550bedad7f830d61
Aleksandr Krivoshein <77492375+Wood85@users.noreply.github.com>
2025-08-11 13:19:15 +0400

    fix: common improvements (#20)
    * docs: fill in README.md file
    
    * fix: common improvements

--- src/pages/PrivacyPolicy/model/getPolicyPoints.ts
+++ src/pages/PrivacyPolicy/model/getPolicyPoints.ts
@@ -1,7 +1,17 @@
 import { getPoints } from '@utils/getPoints';
 import { TFunction } from 'i18next';
 
-export const pointKeys = ['one', 'two', 'three', 'four', 'five', 'seven', 'eight', 'nine'] as const;
+export const pointKeys = [
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine'
+] as const;
 
 export type PointKey = (typeof pointKeys)[number];
 export type PolicyPoints = Record<PointKey, string[]>;
